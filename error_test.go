0213956aecc976cc3dfee527ad21132f2f0f1994
Alfan Mubarok <n4.labs@mailbox.org>
2023-07-29 16:30:29 +0700

    test(error): add test DuplicatedRuleError type

---
+++ error_test.go
@@ -0,0 +1,32 @@
+package blu
+
+import (
+	"testing"
+)
+
+func TestErrorMessages(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name     string
+		err      error
+		expected string
+	}{
+		{
+			name:     "error message: DuplicatedRuleError",
+			err:      &DuplicatedRuleError{RuleName: "alpha_space"},
+			expected: "duplicated rule: rule alpha_space already exist.",
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			if tc.err.Error() != tc.expected {
+				t.Errorf("expected error message: %s, got: %s", tc.expected, tc.err.Error())
+			}
+		})
+	}
+}
