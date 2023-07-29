48d50bbab5583e0b1df53026e4d5d45610ba8112
Alfan Mubarok <n4.labs@mailbox.org>
2023-07-29 19:38:35 +0700

    feat(error): add UnexportedFieldError type

--- error.go
+++ error.go
@@ -5,9 +5,19 @@ type (
 	DuplicatedRuleError struct {
 		RuleName string
 	}
+
+	// UnexportedFieldError an error type for attempting to validate an unexported field.
+	UnexportedFieldError struct {
+		Field string
+	}
 )
 
 // Error returns an error message indicating that a rule with the same name already exists.
 func (e *DuplicatedRuleError) Error() string {
 	return "duplicated rule: rule " + e.RuleName + " already exist."
+}
+
+// Error returns an error message indicating unexported field (lowercase).
+func (e *UnexportedFieldError) Error() string {
+	return "unexported field encountered for field: " + e.Field
 }
