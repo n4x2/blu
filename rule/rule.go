ed470b381a6feeb35a2db11ab13dde02e1905abe
Alfan Mubarok <n4.labs@mailbox.org>
2023-08-05 08:26:22 +0700

    feat(rule): add alpha space regex

--- rule/rule.go
+++ rule/rule.go
@@ -7,15 +7,17 @@ const (
 )
 
 const (
-	alphaDashPattern = "^[a-zA-Z0-9_-]+$"
-	alphaNumPattern  = "^[a-zA-Z0-9]+$"
-	numberPattern    = "^[0-9]+$"
-	numericPattern   = "^[-+]?\\d+(\\.\\d+)?$"
+	alphaDashPattern  = "^[a-zA-Z0-9_-]+$"
+	alphaNumPattern   = "^[a-zA-Z0-9]+$"
+	alphaSpacePattern = "^[a-zA-Z ]+$"
+	numberPattern     = "^[0-9]+$"
+	numericPattern    = "^[-+]?\\d+(\\.\\d+)?$"
 )
 
 var (
-	alphaDashRegex = regexp.MustCompile(alphaDashPattern)
-	alphaNumRegex  = regexp.MustCompile(alphaNumPattern)
-	numberRegex    = regexp.MustCompile(numberPattern)
-	numericRegex   = regexp.MustCompile(numericPattern)
+	alphaDashRegex  = regexp.MustCompile(alphaDashPattern)
+	alphaNumRegex   = regexp.MustCompile(alphaNumPattern)
+	alphaSpaceRegex = regexp.MustCompile(alphaSpacePattern)
+	numberRegex     = regexp.MustCompile(numberPattern)
+	numericRegex    = regexp.MustCompile(numericPattern)
 )
