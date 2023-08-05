b017908e00adddb25455254b1ec68018f2cbe073
Alfan Mubarok <n4.labs@mailbox.org>
2023-08-05 08:17:55 +0700

    feat(rule): add alpha numeric regex

--- rule/rule.go
+++ rule/rule.go
@@ -8,12 +8,14 @@ const (
 
 const (
 	alphaDashPattern = "^[a-zA-Z0-9_-]+$"
+	alphaNumPattern  = "^[a-zA-Z0-9]+$"
 	numberPattern    = "^[0-9]+$"
 	numericPattern   = "^[-+]?\\d+(\\.\\d+)?$"
 )
 
 var (
 	alphaDashRegex = regexp.MustCompile(alphaDashPattern)
+	alphaNumRegex  = regexp.MustCompile(alphaNumPattern)
 	numberRegex    = regexp.MustCompile(numberPattern)
 	numericRegex   = regexp.MustCompile(numericPattern)
 )
